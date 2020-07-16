9e4be74f1ed849a9b0c75b2dd197cc3874db4d80
kenneth irungu <kennethirungu1800@gmail.com>
2020-07-17 02:57:02 +0300

    Added the .env package and configured it

--- src/db.js
+++ src/db.js
@@ -1,10 +1,12 @@
 const db = require("knex")({
     client: "pg",
     connection: {
-        host: "localhost",
-        user: "proff",
-        password: "proff1800",
-        database: "feeds",
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE_NAME,
+
+
     },
 });
 
